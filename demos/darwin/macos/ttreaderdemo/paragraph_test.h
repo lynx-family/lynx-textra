3b39237899948b3929ea3f8bcda7d6e71518cbeb
hfuttyh <hfuttyh@gmail.com>
2025-12-25 17:14:14 +0800

    [BugFix] fix textshadow support

--- demos/darwin/macos/ttreaderdemo/paragraph_test.h
+++ demos/darwin/macos/ttreaderdemo/paragraph_test.h
@@ -153,6 +153,7 @@ class ParagraphTest {
   void TestAlignWithBBox(ICanvasHelper* canvas, float width) const;
   void TestModifyHAlignAfterLayout(ICanvasHelper* canvas, float width) const;
   void TestApplyStyleInRange(ICanvasHelper* canvas, float width) const;
+  void TestTextShadow(ICanvasHelper* canvas, float width) const;
 
  private:
   uint32_t test_width = 490;
